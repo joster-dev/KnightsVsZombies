eede288c54eee97619c76b58de033f8a5e5d703a
Jacob Osterhout <joster422@gmail.com>
2013-03-26 14:26:15 -0400

    High Scores Update Part 1

--- src/HighScore.java
+++ src/HighScore.java
@@ -6,12 +6,16 @@ public class HighScore extends JPanel {
 	
 	public boolean back = false;
 	
-	public Rectangle highScoreListSpace;
-	public String[] highScores = new String[]{"Jim1", "Jim2", "Jim3", "Jim4", "Jim5"};
-	public String highScoreTitle = "High Scores";
+	public Rectangle achievementListSpace;
+	public String achievementTitle = "Achievements";
+	public Rectangle[] achievementBoxes = new Rectangle[6];
+	public boolean[] achievementState = new boolean[6];
 	
-	public Rectangle achivementSpace;
-	public String achivementTitle = "Achivements";
+	public Rectangle highScoreListSpace;
+	public String highScoreTitle = "High Scores";
+	public Rectangle[] highScoreBoxes = new Rectangle[5];
+	public String[] highScoreNames = new String[]{"Ted", "Steve", "Jim", "Tyrone", "Sam"};
+	public int[] highScores = new int[]{1000, 900, 800, 700, 600};
 	
 	public Rectangle imgBox;
 	
@@ -22,10 +26,32 @@ public class HighScore extends JPanel {
 	public HighScore (Frame frame) {
 		myFrame = frame;
 		
-		highScoreListSpace = new Rectangle(Frame.x / 9, Frame.y / 9, Frame.x / 3, (Frame.y * 7) / 9);
-		achivementSpace = new Rectangle((Frame.x * 5) / 9, Frame.y / 9, Frame.x / 3, (Frame.y * 10) / 27);
+		achievementListSpace = new Rectangle(Frame.x / 9, Frame.y / 9, Frame.x / 3, (Frame.y * 7) / 9);
+		highScoreListSpace = new Rectangle((Frame.x * 5) / 9, Frame.y / 9, Frame.x / 3, (Frame.y * 10) / 27);
 		imgBox = new Rectangle((Frame.x * 5) / 9, (Frame.y * 5) / 9, (Frame.x * 4) / 27, Frame.y / 3);
 		backButton = new Rectangle((Frame.x * 20) / 27, (Frame.y * 17) / 27, (Frame.x * 4) / 27, (Frame.y * 5) / 27);
+		
+		for(int i = 0; i < achievementBoxes.length; i++) {
+			achievementBoxes[i] = new Rectangle(achievementListSpace.x + (achievementListSpace.width / 10), achievementListSpace.y + (achievementListSpace.height * 4 / 23) + (achievementListSpace.height * 3 / 23) * i, achievementListSpace.width * 4 / 5, achievementListSpace.height * 3 / 23);
+		}
+		
+		for(int j = 0; j < highScoreBoxes.length; j++) {
+			highScoreBoxes[j] = new Rectangle(highScoreListSpace.x + (highScoreListSpace.width / 10), highScoreListSpace.y + (highScoreListSpace.height / 5) + (highScoreListSpace.height * 3 / 20) * j, highScoreListSpace.width * 4 / 5, highScoreListSpace.height * 3 / 20);
+		}
+	}
+	
+	public void newHighScore(String name, int highScore) {
+		for(int i = 0; i < highScores.length; i++) {
+			if(highScore > highScores[i]) {
+				for(int j = highScores.length - 1; j > i; j--) {
+					highScores[j] = highScores[j - 1];
+					highScoreNames[j] = highScoreNames[j - 1];
+				}
+				highScores[i] = highScore;
+				highScoreNames[i] = name;
+				break;
+			}
+		}
 	}
 	
 	public void click(int mouseclick) {
@@ -43,8 +69,8 @@ public class HighScore extends JPanel {
 		g.fillRect(0, 0, Opening.myWidth, Opening.myHeight);
 		
 		g.setColor(Color.WHITE);
+		g.fillRect(achievementListSpace.x, achievementListSpace.y, achievementListSpace.width, achievementListSpace.height);
 		g.fillRect(highScoreListSpace.x, highScoreListSpace.y, highScoreListSpace.width, highScoreListSpace.height);
-		g.fillRect(achivementSpace.x, achivementSpace.y, achivementSpace.width, achivementSpace.height);
 		
 		g.setColor(Color.GREEN);
 		g.fillRect(imgBox.x, imgBox.y, imgBox.width, imgBox.height);
@@ -53,9 +79,32 @@ public class HighScore extends JPanel {
 		g.fillRect(backButton.x, backButton.y, backButton.width, backButton.height);
 		
 		g.setColor(Color.BLACK);
+		g.drawRect(achievementListSpace.x, achievementListSpace.y, achievementListSpace.width, achievementListSpace.height);
 		g.drawRect(highScoreListSpace.x, highScoreListSpace.y, highScoreListSpace.width, highScoreListSpace.height);
-		g.drawRect(achivementSpace.x, achivementSpace.y, achivementSpace.width, achivementSpace.height);
 		g.drawRect(backButton.x, backButton.y, backButton.width, backButton.height);
+		
+		for(int i = 0; i < achievementBoxes.length; i++) {
+			if(!achievementState[i]) {			//*Sets the opacity to a dark color if the achievement is not unlocked.*//
+				g.setColor(new Color(0, 0, 0, 150));
+				g.fillRect(achievementBoxes[i].x, achievementBoxes[i].y, achievementBoxes[i].width, achievementBoxes[i].height);
+			}									//*//
+			g.setColor(Color.BLACK);
+			g.drawRect(achievementBoxes[i].x, achievementBoxes[i].y, achievementBoxes[i].width, achievementBoxes[i].height);
+		}
+		for(int j = 0; j < highScoreBoxes.length; j++) {
+			g.drawRect(highScoreBoxes[j].x, highScoreBoxes[j].y, highScoreBoxes[j].width, highScoreBoxes[j].height);
+		}
+		
+		g.setFont(new Font("Arial", Font.BOLD, 16));
+		g.drawString(achievementTitle, achievementListSpace.x + (achievementListSpace.width * 3 / 10), achievementListSpace.y + (achievementListSpace.height * 2 / 23) * 4 / 3);
+		g.drawString(highScoreTitle, highScoreListSpace.x + (highScoreListSpace.width * 7 / 20), highScoreListSpace.y + (highScoreListSpace.height / 10) * 4 / 3);
+		
+		g.setFont(new Font("Arial", Font.BOLD, 12));
+		for(int k = 0; k < highScores.length; k++) {
+			g.drawString(Integer.toString(k + 1) + ".", highScoreBoxes[k].x + highScoreListSpace.width / 10, highScoreBoxes[k].y + (highScoreListSpace.height / 10));
+			g.drawString(highScoreNames[k], highScoreBoxes[k].x + highScoreListSpace.width * 3 / 10, highScoreBoxes[k].y + (highScoreListSpace.height / 10));
+			g.drawString(Integer.toString(highScores[k]), highScoreBoxes[k].x + highScoreListSpace.width / 2, highScoreBoxes[k].y + (highScoreListSpace.height / 10));
+		}
 		
 		if(backButton.contains(Opening.mse)) {
 			
